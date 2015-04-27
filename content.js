27b9a5367d57c0519d5a340bfa92af3e363b9b96
Mahmoud Fawzy <m.fawzy.linux@gmail.com>
2015-04-27 14:12:54 +0300

    Added basic click handler support and enhanced popupbehavior

--- content.js
+++ content.js
@@ -5,34 +5,79 @@
 (function(){
     var initialized = false;
     var current_step = 0;
+    var container, popup, popupbody, popupbuttons;
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse){
             if(request.message == "start_bs_helper"){
                 if(initialized == false){
-                    var container = document.createElement('div');
-                    container.className= "_bs-animate";
+                    container = document.createElement('div');
+                    container.classList.add("_bs-animate");
                     container.id = "_bs-main-container";
 
-                    var popup = document.createElement('div');
+                    popup = document.createElement('div');
                     popup.id = "_bs-popup";
-                    popup.innerHTML = '<div id="_bs-close-popup"><a href="javascript;;">[X]</a></div>';
+                    popup.innerHTML = '<div id="_bs-close-popup"><a onclick="this.parentNode.parentNode.parentNode.classList.remove(\'_bs-shown\')">[X]</a></div>';
                     container.appendChild(popup);
 
-                    var popupbody = document.createElement('div');
+                    popupbody = document.createElement('div');
                     popupbody.id = "_bs-popup-body";
-                    popupbody.innerText = "Welcome to BS MediaStrategy Creator";
+                    updatePopupBody(current_step);
                     popup.appendChild(popupbody);
+
+                    popupbuttons = document.createElement('div');
+                    popupbuttons.id = "_bs-popup-buttons";
+                    popupbuttons.innerHTML = '<input type="submit" value="Next" class="_bs-button">';
+                    popup.appendChild(popupbuttons);
 
 
                     document.body.appendChild(container);
-                    container.className += " _bs-shown"
+                    container.classList.add("_bs-shown");
                     //console.log(ret);
                     initialized = true;
+
+                    document.addEventListener('click', function(e){
+                        clickHandler(e)
+                    }, false);
                 }
                 else{
-                    console.log("already initialized!")
+                    container.classList.toggle('_bs-shown');
                 }
             }
         }
-    )
+    );
+
+
+    function clickHandler(e) {
+        if(container.classList.contains("_bs-shown")){
+            e = e || window.event;
+            var target = e.target || e.srcElement,
+                text = target.textContent || target.innerText;
+            target.classList.add('_bs-highlighted');
+        }
+
+    }
+
+    function updatePopupBody(step){
+        switch (step) {
+            case 1:
+                popupbody.innerText = "Please Click the Play button on the website's player";
+                break;
+            case 2:
+                popupbody.innerText = "Please Click the Next button on the website's player";
+                break;
+            case 3:
+                popupbody.innerText = "Please Click the Previous button on the website's player";
+                break;
+            case 4:
+                popupbody.innerText = "Please Click the Pause button on the website's player";
+                break;
+            case 5:
+                popupbody.innerText = "Please Click the Favorite/Like/Love button on the website's player";
+                break;
+            default:
+                popupbody.innerHTML = "<div>Welcome to BS MediaStrategy Creator</div>";
+
+
+        }
+    }
 })();
